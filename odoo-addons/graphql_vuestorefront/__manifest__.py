66f5ac4b83b39cdc68bdc02519efd2a600f3ac40
bfigueiredo <bfigueiredo@odoogap.com>
2021-06-11 18:26:14 +0100

    [IMP] adding new demo data

--- odoo-addons/graphql_vuestorefront/__manifest__.py
+++ odoo-addons/graphql_vuestorefront/__manifest__.py
@@ -25,7 +25,8 @@
         'data/demo_products_women_clothing.xml',
         'data/demo_products_women_shoes.xml',
         'data/demo_products_women_bags.xml',
-        'data/demo_products_men_clothing.xml',
+        'data/demo_products_men_clothing_1.xml',
+        'data/demo_products_men_clothing_2.xml',
         'data/demo_products_men_shoes.xml'
     ],
     'installable': True,
