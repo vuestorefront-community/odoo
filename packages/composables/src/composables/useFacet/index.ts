910c444af336ef67f3f29cc5dbbd8497166d16ee
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-04 15:30:35 -0300

    chore: [IMP] update category composable

--- packages/composables/src/composables/useFacet/index.ts
+++ packages/composables/src/composables/useFacet/index.ts
@@ -1,13 +1,21 @@
-import { Context, useFacetFactory, FacetSearchResult } from '@vue-storefront/core';
+import {
+  Context,
+  useFacetFactory,
+  FacetSearchResult
+} from '@vue-storefront/core';
 
 const factoryParams = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   search: async (context: Context, params: FacetSearchResult<any>) => {
-
-    const categories = await context.$odoo.api.getCategory({ ...params, topCategory: false });
-    const products = await context.$odoo.api.getProductTemplatesPublished({ ...params.input });
+    const { categories } = await context.$odoo.api.getCategory({
+      filter: { parent: true },
+      search: params
+    });
+    const products = await context.$odoo.api.getProductTemplatesPublished({
+      ...params.input
+    });
     return {
-      categories,
+      categories: categories.categories,
       products: products.result?.products,
       attributes: products.result?.attributes,
       totalProducts: products.result?.product_count
