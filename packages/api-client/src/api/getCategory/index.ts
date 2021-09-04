a435e1aecb36cdcdfeec4efb98290baeeba218d6
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-04 15:31:23 -0300

    chore: [IMP] update category api

--- packages/api-client/src/api/getCategory/index.ts
+++ packages/api-client/src/api/getCategory/index.ts
@@ -3,23 +3,21 @@ import { Context, CustomQuery } from '@vue-storefront/core';
 import gql from 'graphql-tag';
 import ApolloClient from 'apollo-client';
 import query from './getCategoryQuery';
-import { Category, GraphQlGetCategoryParams } from '../../types';
+import { CategoryResult, GraphQlGetCategoryParams } from '../../types';
 import { FetchResult } from 'apollo-link/lib/types';
 
 export default async function getCategory(
   context: Context,
   params: GraphQlGetCategoryParams,
   customQuery?: CustomQuery
-): Promise<FetchResult<Category>> {
+): Promise<FetchResult<CategoryResult>> {
   const apolloClient = context.client.apollo as ApolloClient<any>;
 
   const response = await apolloClient.query({
-    query: gql`
-      ${query}
-    `,
+    query,
     fetchPolicy: 'no-cache',
     variables: params
   });
 
-  return response.data.allEcommerceCategories;
+  return response.data;
 }
