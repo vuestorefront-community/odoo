ab704b0e5b1ffc9370df76c07d09517b9c3a9a8a
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-04 20:56:45 -0300

    chore: [IMP] update eslint rule

--- packages/composables/src/composables/useFacet/index.ts
+++ packages/composables/src/composables/useFacet/index.ts
@@ -8,12 +8,12 @@ const factoryParams = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   search: async (context: Context, params: FacetSearchResult<any>) => {
     const { categories } = await context.$odoo.api.getCategory({
-      filter: { parent: true },
-      search: params
+      filter: { parent: false }
     });
     const products = await context.$odoo.api.getProductTemplatesPublished({
       ...params.input
     });
+
     return {
       categories: categories.categories,
       products: products.result?.products,
