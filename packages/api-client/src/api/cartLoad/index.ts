30910952abf4303b294d184970011ec0bab88a03
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-04 16:00:12 -0300

    chore: [IMP] update cart load types

--- packages/api-client/src/api/cartLoad/index.ts
+++ packages/api-client/src/api/cartLoad/index.ts
@@ -2,14 +2,14 @@
 import { Context, CustomQuery } from '@vue-storefront/core';
 import query from './cartLoadQuery';
 import ApolloClient from 'apollo-client';
-import { SaleOrder } from '../../types';
+import { Cart } from '../../types';
 import { FetchResult } from 'apollo-link/lib/types';
 
 export default async function cartLoad(
   context: Context,
   params: Record<string, string>,
   customQuery?: CustomQuery
-): Promise<FetchResult<SaleOrder>> {
+): Promise<FetchResult<Cart>> {
   const apolloClient = context.client.apollo as ApolloClient<any>;
 
   const response = await apolloClient.query({
