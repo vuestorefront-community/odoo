c2c01eb32c09e4269730b44af615f4fa00a8d2c5
munizlrmp <munizlrmp@fab.mil.br>
2021-09-06 16:19:17 -0300

    refactor: [IMP] added required method

--- packages/composables/src/composables/getters/orderGetters.ts
+++ packages/composables/src/composables/getters/orderGetters.ts
@@ -4,36 +4,28 @@
 import { UserOrderGetters } from '@vue-storefront/core';
 import { Order, OrderItem } from '../types';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getDate = (order: any): string => order?.date || '123';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getId = (order: any): string =>
   order?.id || Math.floor(Math.random() * 100);
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getStatus = (order: any): string => order?.status || 'Failed';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getPrice = (order: any): number | null => order?.price || 0;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getItems = (order: any): any[] => order?.items || [];
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getItemSku = (item: any): string => item?.sku || 0;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getItemName = (item: any): string => item?.name || 0;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getItemQty = (item: any): number => item?.qty || 0;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getItemPrice = (item: any): number => item?.price?.current || 0;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getFormattedPrice = (price: number) => String(price);
+
+export const getOrdersTotal = (orders: any) => orders.total;
 
 const orderGetters: UserOrderGetters<Order, OrderItem> = {
   getDate,
@@ -45,7 +37,8 @@ const orderGetters: UserOrderGetters<Order, OrderItem> = {
   getItemName,
   getItemQty,
   getItemPrice,
-  getFormattedPrice
+  getFormattedPrice,
+  getOrdersTotal
 };
 
 export default orderGetters;
