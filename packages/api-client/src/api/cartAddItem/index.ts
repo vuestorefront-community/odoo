c553403e0804fb07fdf95478ba86b385d8198674
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-07 13:19:39 -0300

    refactor: [IMP] update cartAddItem to graphql api

--- packages/api-client/src/api/cartAddItem/index.ts
+++ packages/api-client/src/api/cartAddItem/index.ts
@@ -17,5 +17,5 @@ export default async function cartAddItem(
     variables: params
   });
 
-  return response;
+  return response.data;
 }
--- packages/api-client/src/api/cartLoad/cartLoadQuery.ts
+++ packages/api-client/src/api/cartLoad/cartLoadQuery.ts
@@ -1,51 +1,10 @@
 import gql from 'graphql-tag';
+import orderFragment from '../../fragments/orderFragment';
 
 export default gql`
   query {
     cart {
-      order {
-        name
-        amountTotal
-        amountTax
-        orderLines {
-          id
-          product {
-            id
-            name
-            image
-          }
-          quantity
-          priceTotal
-        }
-        partnerInvoice {
-          id
-          name
-          street
-          city
-          phone
-          zip
-          country {
-            id
-          }
-          state {
-            id
-          }
-        }
-        partnerShipping {
-          id
-          name
-          street
-          city
-          phone
-          zip
-          country {
-            id
-          }
-          state {
-            id
-          }
-        }
-      }
+      ${orderFragment}
     }
   }
 `;
