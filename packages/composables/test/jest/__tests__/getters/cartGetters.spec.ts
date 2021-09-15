db20851cfd8b8b8df746df671c3e25ff15c97cb9
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-14 23:06:19 -0300

    refactor: [IMP] update cart getter test

--- packages/composables/test/jest/__tests__/getters/cartGetters.spec.ts
+++ packages/composables/test/jest/__tests__/getters/cartGetters.spec.ts
@@ -1,68 +1,13 @@
 import cartGetters from '../../../../src/composables/getters/cartGetters';
-
+import { mockedCart } from '../__mocks__/mockedCart';
 it('calculate cart subtotal', () => {
-  const cart = {
-    id: 1,
-    name: '',
-    amountTotal: 151,
-    amountDelivery: 10
-  };
-  const categoryTree = cartGetters.getTotals(cart);
+  const categoryTree = cartGetters.getTotals(mockedCart);
 
-  expect(categoryTree).toStrictEqual({ subtotal: 141, total: 151 });
+  expect(categoryTree).toStrictEqual({ subtotal: 1639.75, total: 1643.75 });
 });
 
 it('cart total items', () => {
-  const cart = {
-    id: 1,
-    name: '',
-    amountTotal: 151.60,
-    amountDelivery: 10.30,
-    websiteOrderLine: new Array(30)
-  };
-  const totalItems = cartGetters.getTotalItems(cart);
+  const totalItems = cartGetters.getTotalItems(mockedCart);
 
-  expect(totalItems).toBe(30);
+  expect(totalItems).toBe(4);
 });
-
-it('empty cart items', () => {
-  const cart = {
-    id: 1,
-    name: '',
-    amountTotal: 151.60,
-    amountDelivery: 10.30
-  };
-  const totalItems = cartGetters.getTotalItems(cart);
-
-  expect(totalItems).toBe(0);
-});
-
-it('cart items', () => {
-  const cart = {
-    id: 1,
-    name: '',
-    amountTotal: 151.60,
-    amountDelivery: 10.30,
-    websiteOrderLine: [
-      { id: 1 },
-      { id: 2 }
-    ]
-  };
-  const cartItems = cartGetters.getItems(cart);
-
-  expect(cartItems).toStrictEqual([{ id: 1 }, { id: 2 }]);
-});
-
-it('empty cart items', () => {
-  const cart = {
-    id: 1,
-    name: '',
-    amountTotal: 151.60,
-    amountDelivery: 10.30,
-    websiteOrderLine: []
-  };
-  const cartItems = cartGetters.getItems(cart);
-
-  expect(cartItems).toStrictEqual([]);
-});
-
