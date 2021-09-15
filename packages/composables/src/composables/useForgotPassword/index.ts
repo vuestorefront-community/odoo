9afa1798b33e55fd071fbe824a8b6880a2fde8f2
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-14 22:42:01 -0300

    refactor: [IMP] add new password manage factory from 2.4

---
+++ packages/composables/src/composables/useForgotPassword/index.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Context,
+  useForgotPasswordFactory,
+  UseForgotPasswordFactoryParams
+} from '@vue-storefront/core';
+
+const factoryParams: UseForgotPasswordFactoryParams<any> = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  resetPassword: async (context: Context, { email, customQuery }) => {
+    const { data } = await context.$odoo.api.sendResetPassword(email);
+
+    return data;
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  setNewPassword: async (
+    context: Context,
+    { tokenValue, newPassword, customQuery }
+  ) => {
+    console.log('Mocked: setNewPassword');
+    return {};
+  }
+};
+
+export const useForgotPassword = useForgotPasswordFactory<any>(factoryParams);
+export default useForgotPassword;
