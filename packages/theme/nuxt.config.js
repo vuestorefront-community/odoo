1d0bd9b7b57c83fd74f99472cf54de71d7dcb392
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-21 17:52:13 -0300

    chore: [IMP] update nuxt scrolbehavior config

--- packages/theme/nuxt.config.js
+++ packages/theme/nuxt.config.js
@@ -199,15 +199,5 @@ export default {
         })
       })
     ]
-  },
-
-  router: {
-    scrollBehavior(_to, _from, savedPosition) {
-      if (savedPosition) {
-        return savedPosition;
-      } else {
-        return { x: 0, y: 0 };
-      }
-    }
   }
 };
