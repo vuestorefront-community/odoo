b1927eed6cc8784406b7ecfa68439fdbe4e7f252
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-23 08:27:52 -0300

    feat: [IMP] add adyen external / normal provider components

---
+++ packages/theme/components/Checkout/AdyenPaymentProvider.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    adyenPaymentProvider
+  </div>
+</template>
+
+<script>
+import { SfButton, SfRadio } from '@storefront-ui/vue';
+
+export default {
+  name: 'VsfPaymentProvider',
+
+  components: {
+    SfButton,
+    SfRadio
+  },
+
+  setup() {
+
+    return {
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.shipping {
+  &__label {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__description {
+    --radio-description-margin: 0;
+    --radio-description-font-size: var(--font-xs);
+  }
+}
+</style>
