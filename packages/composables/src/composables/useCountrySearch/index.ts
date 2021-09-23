38e9d53a382baeddfaf7ca7e4d5de126d775ddf1
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-23 10:38:53 -0300

    fix: [FIX] country state param filter composable

--- packages/composables/src/composables/useCountrySearch/index.ts
+++ packages/composables/src/composables/useCountrySearch/index.ts
@@ -2,7 +2,7 @@
 import { ref } from '@vue/composition-api';
 import { useVSFContext } from '@vue-storefront/core';
 import { Context } from '@vue-storefront/core';
-import { GraphQlGetAllCountryStatesParams } from '@vue-storefront/odoo-api/src/types';
+import { GraphQlGetCountryParams } from '@vue-storefront/odoo-api/src/types';
 
 const useCountrySearch = (): any => {
   const context: Context = useVSFContext();
@@ -25,17 +25,17 @@ const useCountrySearch = (): any => {
   const searchCountryStates = async (countryId) => {
     if (!countryId) return;
 
-    const params: GraphQlGetAllCountryStatesParams = {
-      filter: { id: parseInt(countryId) }
+    const params: GraphQlGetCountryParams = {
+      id: parseInt(countryId)
     };
 
-    const response = await context.$odoo.api
+    const { country } = await context.$odoo.api
       .getCountryStates(params)
       .catch((error) => {
         errors.value = error;
       });
 
-    countryStates.value = response?.countries?.countries[1]?.states || [];
+    countryStates.value = country.states;
   };
 
   return {
