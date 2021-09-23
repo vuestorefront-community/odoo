499d34f26b71b32ee57da0a3d412c3ff7c1695e1
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-22 22:55:41 -0300

    feat: [IMP] payment factory and implementation

---
+++ packages/composables/src/factories/usePaymentFactory.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Context, FactoryParams, PlatformApi, Composable, CustomQuery, configureFactoryParams} from '@vue-storefront/core';
+export interface UsePaymentErrors {
+  getPaymentProviderList: Error;
+}
+
+export interface UsePayment<PAYMENT_PROVIDER, API extends PlatformApi = any> extends Composable<API> {
+  getPaymentProviderList(providerList: PAYMENT_PROVIDER): PAYMENT_PROVIDER[];
+}
+
+export interface UsePaymentFactoryParams< PAYMENT_PROVIDER, API extends PlatformApi = any> extends FactoryParams<API> {
+  getPaymentProviderList: (context: Context, params: {
+     customQuery?: CustomQuery;
+    }) => PAYMENT_PROVIDER[];
+}
+export const usePaymentFactory = <PAYMENT_PROVIDER, API extends PlatformApi = any>(
+  factoryParams: UsePaymentFactoryParams<PAYMENT_PROVIDER, API>
+) => function usePayment(): UsePayment<PAYMENT_PROVIDER, API> {
+    const _factoryParams = configureFactoryParams(factoryParams);
+
+    const getPaymentProviderList = _factoryParams.getPaymentProviderList;
+
+    return {
+      getPaymentProviderList
+    };
+  };
+
