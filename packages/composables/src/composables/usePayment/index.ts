24a41affe0009b336957aa4a1e2be3ab11cce624
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-23 11:29:42 -0300

    perf: [IMP] removing load cart on each step of checkout

--- packages/composables/src/composables/usePayment/index.ts
+++ packages/composables/src/composables/usePayment/index.ts
@@ -1,21 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Context } from '@vue-storefront/core';
 import { PaymentProvider, Provider } from '@vue-storefront/odoo-api/src/types';
-import { usePaymentFactory, UsePaymentFactoryParams} from '../../factories/usePaymentFactory';
+import {
+  usePaymentFactory,
+  UsePaymentFactoryParams
+} from '../../factories/usePaymentFactory';
 
 const factoryParams: UsePaymentFactoryParams<PaymentProvider, any> = {
-  getPaymentProviderList: (context: Context<any, any, any>, params: { customQuery?: any }): PaymentProvider[] => {
-
-    return [{
-      id: 1,
-      name: 'AdyenPaymentProvider',
-      provider: Provider.CustomPaymentForm
-    },
-    {
-      id: 2,
-      name: 'AdyenExternalPaymentProvider',
-      provider: Provider.CustomPaymentForm
-    }];
+  getPaymentProviderList: (
+    context: Context<any, any, any>,
+    params: { customQuery?: any }
+  ): PaymentProvider[] => {
+    return [
+      {
+        id: 1,
+        name: 'Adyen',
+        component: 'AdyenPaymentProvider',
+        provider: Provider.CustomPaymentForm
+      },
+      {
+        id: 2,
+        name: 'Adyen external',
+        component: 'AdyenExternalPaymentProvider',
+        provider: Provider.CustomPaymentForm
+      }
+    ];
   }
 };
 
