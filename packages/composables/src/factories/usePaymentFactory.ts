552dbf9dec1821135104cf2e0c76471af8db4672
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-24 08:59:15 -0300

    fix: [FIX] payment factory lint

--- packages/composables/src/factories/usePaymentFactory.ts
+++ packages/composables/src/factories/usePaymentFactory.ts
@@ -1,28 +1,59 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Context, FactoryParams, PlatformApi, Composable, CustomQuery, configureFactoryParams} from '@vue-storefront/core';
-export interface UsePaymentErrors {
-  getPaymentProviderList: Error;
-}
+import { Context, FactoryParams, PlatformApi, Composable, CustomQuery,
+  configureFactoryParams, sharedRef, ComputedProperty, Logger} from '@vue-storefront/core';
+import { computed, Ref } from '@vue/composition-api';
+export interface UsePaymentErrors { getPaymentProviderList: Error;}
 
-export interface UsePayment<PAYMENT_PROVIDER, API extends PlatformApi = any> extends Composable<API> {
-  getPaymentProviderList(providerList: PAYMENT_PROVIDER): PAYMENT_PROVIDER[];
+export interface UsePayment<PAYMENT_PROVIDER, API extends PlatformApi = any>
+  extends Composable<API> {
+  error: ComputedProperty<UsePaymentErrors>;
+  loading: ComputedProperty<boolean>;
+  providerList: Ref<PAYMENT_PROVIDER[]>;
+
+  getPaymentProviderList(
+    providerList: PAYMENT_PROVIDER
+  ): Promise<PAYMENT_PROVIDER[]>;
 }
 
 export interface UsePaymentFactoryParams< PAYMENT_PROVIDER, API extends PlatformApi = any> extends FactoryParams<API> {
-  getPaymentProviderList: (context: Context, params: {
-     customQuery?: CustomQuery;
-    }) => PAYMENT_PROVIDER[];
+
+  getPaymentProviderList: (context: Context, params: { customQuery?: CustomQuery; }) => Promise<PAYMENT_PROVIDER[]>;
 }
 
-export const usePaymentFactory = <PAYMENT_PROVIDER, API extends PlatformApi = any>(
-  factoryParams: UsePaymentFactoryParams<PAYMENT_PROVIDER, API>
-) => function usePayment(): UsePayment<PAYMENT_PROVIDER, API> {
-    const _factoryParams = configureFactoryParams(factoryParams);
+export const usePaymentFactory = < PAYMENT_PROVIDER, API extends PlatformApi = any>(
+  factoryParams: UsePaymentFactoryParams<PAYMENT_PROVIDER, API>) =>
 
-    const getPaymentProviderList = _factoryParams.getPaymentProviderList;
+    function usePayment(id: string): UsePayment<PAYMENT_PROVIDER, API> {
 
-    return {
-      getPaymentProviderList
+      const ssrKey = id || 'usePayment';
+      const _factoryParams = configureFactoryParams(factoryParams);
+
+      const providerList = sharedRef(null, `${ssrKey}-providerList`);
+      const loading = sharedRef<boolean>(false, `${ssrKey}-loading`);
+      const error = sharedRef<UsePaymentErrors>(
+        { getPaymentProviderList: null },
+        `${ssrKey}-error`
+      );
+
+      const getPaymentProviderList = async (): Promise<PAYMENT_PROVIDER[]> => {
+        try {
+          const response = await _factoryParams.getPaymentProviderList();
+
+          providerList.value = response;
+
+          return response;
+        } catch (error) {
+          error.value.Logger.error(
+            `UsePayment/${id}/getPaymentProviderList`,
+            error
+          );
+        }
+      };
+
+      return {
+        loading: computed(() => loading.value),
+        error: computed(() => error.value),
+        providerList,
+        getPaymentProviderList
+      };
     };
-  };
-
