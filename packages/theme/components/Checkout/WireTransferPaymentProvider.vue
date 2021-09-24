f24d4c9f3ceec13f89935282030f76b27ee3d4ea
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-24 07:17:28 -0300

    feat: [IMP] vsf provider components

---
+++ packages/theme/components/Checkout/WireTransferPaymentProvider.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    WireTransferPaymentProvider
+  </div>
+</template>
+
+<script>
+import { SfButton, SfRadio } from '@storefront-ui/vue';
+
+export default {
+  name: 'WireTransferPaymentProvider',
+
+  components: {
+    SfButton,
+    SfRadio
+  },
+
+  setup() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
