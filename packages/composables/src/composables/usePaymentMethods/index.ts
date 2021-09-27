b01def21c18ddc4bbb666abbf00f7fb41f3c54bc
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-27 19:17:46 -0300

    refactor: [IMP] added different composable for payment methods

---
+++ packages/composables/src/composables/usePaymentMethods/index.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-prototype-builtins */
+import { Context, useVSFContext } from '@vue-storefront/core';
+import { ref } from '@vue/composition-api';
+
+const useShipping = (): any => {
+  const context: Context = useVSFContext();
+
+  const errors = ref({ graphQLErrors: [] });
+
+  const shippingMethods = ref([]);
+
+  const resetCountryErrors = () => (errors.value = { graphQLErrors: [] });
+
+  const searchShippingMethods = async () => {
+    if (shippingMethods.value.length > 0) {
+      return shippingMethods;
+    }
+
+    const response = await context.$odoo.api.shippingGetDeliveryMethods();
+
+    shippingMethods.value = response.deliveryMethods.map((method) => ({
+      ...method,
+      id: String(method.id)
+    }));
+  };
+
+  return {
+    resetCountryErrors,
+    searchShippingMethods,
+    shippingMethods,
+    errors
+  };
+};
+
+export default useShipping;
