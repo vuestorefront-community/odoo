6c72536f374ab626813efb000b4c289d61749f66
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-28 20:38:37 -0300

    chore: [IMP] added some nuxt modules to improve performace

--- packages/theme/middleware/checkout.js
+++ packages/theme/middleware/checkout.js
@@ -1,25 +1,31 @@
-const canEnterPayment = (cart) => cart.shippingInfo && cart.shippingAddress;
+const canEnterShipping = (cart) => cart?.order.orderLines?.length > 0 || false;
 
-const canEnterReview = (cart) => Boolean(cart.billingAddress);
+const canEnterBiling = (cart) => canEnterShipping(cart) && cart?.order.partnerShipping.id;
+
+const canEnterPayment = (cart) => canEnterShipping(cart) && canEnterBiling(cart) && cart?.order.partnerInvoice.id;
 
 export default async ({ app, $vsf }) => {
   const currentPath = app.context.route.fullPath.split('/checkout/')[1];
 
   if (!currentPath) return;
 
-  const { data } = await $vsf.$ct.api.getMe();
+  const { cart } = await $vsf.$odoo.api.cartLoad();
 
-  if (!data || !data.me.activeCart) return;
-  const { activeCart } = data.me;
+  if (!cart) return;
 
   switch (currentPath) {
+    case 'shipping':
+      if (!canEnterShipping(cart)) {
+        app.context.redirect('/');
+      }
+      break;
     case 'billing':
-      if (!canEnterPayment(activeCart)) {
+      if (!canEnterBiling(cart)) {
         app.context.redirect('/');
       }
       break;
     case 'payment':
-      if (!canEnterReview(activeCart)) {
+      if (!canEnterPayment(cart)) {
         app.context.redirect('/');
       }
       break;
