440bd8320aec0075b2abbdcdb50e74d2ee9832fc
Carlos Fonseca <cfonseca@odoogap.com>
2021-09-29 14:27:58 +0100

    [ADD] payment acquirer response

--- odoo-addons/14.0/graphql_vuestorefront/controllers/main.py
+++ odoo-addons/14.0/graphql_vuestorefront/controllers/main.py
@@ -1,14 +1,32 @@
 # Copyright 2021 ODOOGAP/PROMPTEQUATION LDA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import werkzeug
 from odoo import http
 from odoo.addons.graphql_base import GraphQLControllerMixin
+from odoo.addons.http_routing.models.ir_http import slug
+from odoo.addons.payment.controllers.portal import PaymentProcessing
+from odoo.addons.payment_adyen.controllers.main import AdyenController
 from odoo.addons.sale.controllers.variant import VariantController
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.http import request
-from odoo.addons.http_routing.models.ir_http import slug
 
 from ..schema import schema
+
+
+class VSFAdyenController(AdyenController):
+
+    @http.route(['/payment/adyen/return'], type='http', auth='public', csrf=False)
+    def adyen_return(self, **post):
+        # Confirm payment transaction
+        super(VSFAdyenController, self).adyen_return(**post)
+
+        # Confirm sale order
+        PaymentProcessing().payment_status_poll()
+
+        # Redirect to VSF
+        vsf_payment_return_url = request.env['ir.config_parameter'].sudo().get_param('vsf_payment_return_url', '')
+        return werkzeug.utils.redirect(vsf_payment_return_url)
 
 
 class GraphQLController(http.Controller, GraphQLControllerMixin):
