a2ec17caae7999ac9ee1dcf9c8cc4a631b962c99
Leonardo Muniz <lmuniz@odoogap.com>
2021-09-29 19:14:34 -0300

    refactor: [IMP] update paymetn factory for confirmation

--- packages/composables/src/composables/usePayment/index.ts
+++ packages/composables/src/composables/usePayment/index.ts
@@ -1,15 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Context } from '@vue-storefront/core';
-import { PaymentProvider } from '@vue-storefront/odoo-api/src/types';
+import { Order, PaymentProvider } from '@vue-storefront/odoo-api/src/types';
 import { usePaymentFactory, UsePaymentFactoryParams} from '../../factories/usePaymentFactory';
 
-const factoryParams: UsePaymentFactoryParams<PaymentProvider, any> = {
+const factoryParams: UsePaymentFactoryParams<PaymentProvider, Order> = {
   getPaymentProviderList: async (context: Context): Promise<PaymentProvider[]> => {
 
     const { paymentAcquirers }: { paymentAcquirers: PaymentProvider[] } = await context.$odoo.api.paymentLoadProviders();
 
     return paymentAcquirers;
+  },
+  getPaymentConfirmation: async (context: Context): Promise<Order> => {
+    const { paymentConfirmation } = await context.$odoo.api.paymentConfirmation();
+    console.log(paymentConfirmation);
+
+    return paymentConfirmation;
   }
+
 };
 
-export default usePaymentFactory<PaymentProvider>(factoryParams);
+export default usePaymentFactory<PaymentProvider, Order>(factoryParams);
