05ab25e15ba680a497e33e63de7a741c4f6abba0
Leonardo Muniz <lmuniz@odoogap.com>
2021-10-01 08:41:24 -0300

    feat: [IMP] add cart getters order / stage

--- packages/composables/src/composables/usePayment/index.ts
+++ packages/composables/src/composables/usePayment/index.ts
@@ -2,9 +2,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Context } from '@vue-storefront/core';
 import { Order, PaymentProvider } from '@vue-storefront/odoo-api/src/types';
-import { usePaymentFactory, UsePaymentFactoryParams} from '../../factories/usePaymentFactory';
+import { usePaymentFactory, UsePaymentFactoryParams } from '../../factories/usePaymentFactory';
+import useCart from '../useCart';
 
 const factoryParams: UsePaymentFactoryParams<PaymentProvider, Order> = {
+  provide() {
+    return {
+      useCart: useCart()
+    };
+  },
+
   getPaymentProviderList: async (context: Context): Promise<PaymentProvider[]> => {
 
     const { paymentAcquirers }: { paymentAcquirers: PaymentProvider[] } = await context.$odoo.api.paymentLoadProviders();
@@ -13,6 +20,8 @@ const factoryParams: UsePaymentFactoryParams<PaymentProvider, Order> = {
   },
   getPaymentConfirmation: async (context: Context): Promise<Order> => {
     const { paymentConfirmation } = await context.$odoo.api.paymentConfirmation();
+
+    context.useCart.cart.value = paymentConfirmation;
 
     return paymentConfirmation;
   }
