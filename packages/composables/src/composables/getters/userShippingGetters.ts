fd0945dcc69da9a469eb3842b18442f16ffcbd00
Leonardo Muniz <lmuniz@odoogap.com>
2021-10-06 22:27:06 -0300

    feat: [ADD] shipping address composables

--- packages/composables/src/composables/getters/userShippingGetters.ts
+++ packages/composables/src/composables/getters/userShippingGetters.ts
@@ -1,28 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { UserShippingGetters } from '@vue-storefront/core';
 
 const userGetters: UserShippingGetters<any, any> = {
   getAddresses: (shipping, criteria?: Record<string, any>) => {
-    const { addresses } = shipping;
-    if (!criteria || !Object.keys(criteria).length) {
-      return addresses;
-    }
-    const entries = Object.entries(criteria);
-    return shipping.addresses.filter(
-      address => entries.every(([key, value]) => address[key] === value)
-    );
+    return shipping;
   },
   getDefault: shipping => shipping.addresses.find(address => address.isDefault),
   getTotal: shipping => shipping.addresses.length,
-  getPostCode: address => address ? address.zipCode : '',
-  getStreetName: address => address ? address.streetName : '',
+  getPostCode: address => address ? address.zip : '',
+  getStreetName: address => address ? address.street : '',
   getStreetNumber: address => address ? address.streetNumber : '',
   getCity: address => address ? address.city : '',
-  getFirstName: address => address ? address.firstName : '',
+  getFirstName: address => address ? address.name : '',
   getLastName: address => address ? address.lastName : '',
-  getCountry: address => address ? address.country : '',
-  getPhone: address => address ? address.phoneNumber : '',
+  getCountry: address => address ? address.country.name : '',
+  getPhone: address => address ? address.phone : '',
   getEmail: address => address ? address.email : '',
-  getProvince: address => address ? address.state : '',
+  getProvince: address => address ? address.state.name : '',
   getCompanyName: address => address ? address.company : '',
   getTaxNumber: address => address ? address.taxId : '',
   getId: address => address ? address.id : '',
