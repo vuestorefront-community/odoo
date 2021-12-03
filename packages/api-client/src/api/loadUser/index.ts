fde5e68e5f2fbfd5fc0ff96cba7bc6a1ca6b54f4
Leonardo Muniz <lmuniz@odoogap.com>
2021-12-03 12:04:37 -0300

    fix: [FIX] load user not allow custom query

--- packages/api-client/src/api/loadUser/index.ts
+++ packages/api-client/src/api/loadUser/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Context, CustomQuery } from '@vue-storefront/core';
 import ApolloClient from 'apollo-client';
+import gql from 'graphql-tag';
 import query from './loadUserQuery';
 import { FetchResult } from 'apollo-link/lib/types';
 import { LoadUserResult } from '../../types';
@@ -10,20 +11,14 @@ export default async function loadUser(
 ): Promise<FetchResult<LoadUserResult>> {
   const apolloClient = context.client.apollo as ApolloClient<any>;
 
-  try {
-    const response = await apolloClient.query({
-      query,
-      fetchPolicy: 'no-cache'
-    });
+  const { loadUser } = context.extendQuery(
+    customQuery, { loadUser: { query } }
+  );
 
-    return response;
-  } catch (error) {
-    if (error.graphQLErrors) {
-      return {
-        errors: error.graphQLErrors,
-        data: null
-      };
-    }
-    throw error.networkError?.result || error;
-  }
+  return await apolloClient.query({
+    query: gql`${loadUser.query}`,
+    variables: loadUser.variables,
+    fetchPolicy: 'no-cache'
+  });
+
 }
