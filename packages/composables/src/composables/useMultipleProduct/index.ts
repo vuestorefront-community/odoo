ed31d01d2896f22fac4348c31df75c7dcc06d463
Leonardo Muniz <lmuniz@odoogap.com>
2022-03-24 22:46:08 -0300

    fix: product add multiple not loading cart

--- packages/composables/src/composables/useMultipleProduct/index.ts
+++ packages/composables/src/composables/useMultipleProduct/index.ts
@@ -1,8 +1,14 @@
 import { Context, CustomQuery} from '@vue-storefront/core';
 import { Product, GraphQlAddMultipleProductsParams, Cart } from '@vue-storefront/odoo-api';
 import { useMultipleProductFactory, UseMultipleProductFactoryParams } from '../../factories/useMultipleProductFactory';
+import useCart from '../useCart';
 
 const params: UseMultipleProductFactoryParams<Product, GraphQlAddMultipleProductsParams, Cart> = {
+  provide() {
+    return {
+      useCart: useCart()
+    };
+  },
 
   addMultipleProductsToCart: async (context: Context, params: GraphQlAddMultipleProductsParams & { customQuery?: CustomQuery }): Promise<Cart> => {
 
@@ -10,6 +16,7 @@ const params: UseMultipleProductFactoryParams<Product, GraphQlAddMultipleProduct
 
     const { data } = await context.$odoo.api.cartAddMulipleItems(params, customQuery);
 
+    context.useCart.setCart(data.cartAddMultipleItems);
     return data.cartAddMultipleItems;
   }
 };
