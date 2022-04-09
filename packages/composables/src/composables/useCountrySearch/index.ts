87d4a5936acd5fffcee973d6853e6c2d6a7ad4e7
Leonardo Muniz <lmuniz@odoogap.com>
2022-04-09 12:55:16 -0300

    fix: add ssrRef on country composable

--- packages/composables/src/composables/useCountrySearch/index.ts
+++ packages/composables/src/composables/useCountrySearch/index.ts
@@ -1,16 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { ref } from '@nuxtjs/composition-api';
+import { ref, ssrRef } from '@nuxtjs/composition-api';
 import { useVSFContext } from '@vue-storefront/core';
 import { Context } from '@vue-storefront/core';
 import { GraphQlGetCountryParams } from '@vue-storefront/odoo-api';
 
-const useCountrySearch = (): any => {
+const useCountrySearch = (newKey: string): any => {
+  const key = 'country' || newKey;
   const context: Context = useVSFContext();
 
   const errors = ref({ graphQLErrors: [] });
 
-  const countries = ref([]);
-  const countryStates = ref([]);
+  const countries = ssrRef([], key);
+  const countryStates = ssrRef([], key);
 
   const resetCountryErrors = () => (errors.value = { graphQLErrors: [] });
 
