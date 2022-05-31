30dd3dc1107cc7f3ed67d7c186144f47e238fea3
Leonardo Muniz <lmuniz@odoogap.com>
2022-05-31 19:09:11 -0300

    feat: add payment confirmation custom query

--- packages/api-client/src/api/paymentConfirmation/index.ts
+++ packages/api-client/src/api/paymentConfirmation/index.ts
@@ -1,5 +1,6 @@
-/* eslint-disable camelcase */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import gql from 'graphql-tag';
 import { Context, CustomQuery } from '@vue-storefront/core';
 import query from './paymentConfirmationQuery';
 import { FetchResult } from 'apollo-link/lib/types';
@@ -7,12 +8,19 @@ import ApolloClient from 'apollo-client';
 
 export default async function paymentConfirmation(
   context: Context,
+  params: any,
   customQuery?: CustomQuery
 ): Promise<FetchResult> {
   const apolloClient = context.client.apollo as ApolloClient<any>;
 
+  const { paymentConfirmation } = context.extendQuery(
+    customQuery, { paymentConfirmation: { query, variables: params } }
+  );
+
   const response = await apolloClient.query({
-    query
+    query: gql`${paymentConfirmation.query}`,
+    variables: paymentConfirmation.variables,
+    errorPolicy: 'all'
   });
 
   return response;
