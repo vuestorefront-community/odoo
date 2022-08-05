fdc422817f0ac17856cae937554e30b9d9c78d74
Leonardo Muniz <lmuniz@odoogap.com>
2022-08-05 14:01:41 -0300

    chore: removed default theme

--- packages/theme/plugins/getImage.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-export default ({ app }, inject) => {
-  const getImage = (imagePath: string) => {
-    return `${app.$config.baseURL}${imagePath?.replace('/', '')}`;
-  };
-  inject('image', getImage);
-};
