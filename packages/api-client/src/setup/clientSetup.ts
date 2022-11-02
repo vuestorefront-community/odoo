e3886fe3212099d391458fd5d8db28721f94bc75
Leonardo Muniz <lmuniz@odoogap.com>
2022-11-02 14:43:55 -0300

    fix: remove unused log

--- packages/api-client/src/setup/clientSetup.ts
+++ packages/api-client/src/setup/clientSetup.ts
@@ -7,13 +7,6 @@ const onCreate = (settings: Config): { config: Config; client: ClientInstance } 
 
   const config = (settings as any) as Config;
 
-  // if (config.payment?.providers.length === 0) {
-  //   console.warn(
-  //     '%c [Config error]: Message: You must set payment providers on middleware config',
-  //     'background: #222; color: #FFA07A'
-  //   );
-  // }
-
   const { apolloLink } = createOddoLink(config);
 
   const apolloClient = new ApolloClient({
