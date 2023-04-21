1a469da39228a7fea8c9d8ab4ee85e1352b81d56
Leonardo Muniz <lmuniz@odoogap.com>
2023-04-21 16:13:54 -0300

    feat: add cache to all categories api

--- packages/api-client/src/api/getCategories/index.ts
+++ packages/api-client/src/api/getCategories/index.ts
@@ -5,13 +5,22 @@ import { FetchResult } from 'apollo-link';
 import gql from 'graphql-tag';
 import { CategoriesResult, GraphQlGetCategoriesParams } from '../../index';
 import query from './getCategoriesQuery';
+import { randomIntegerBetween } from '../../';
 
 export default async function getCategories(
   context: Context,
   params: GraphQlGetCategoriesParams,
   customQuery?: CustomQuery
 ): Promise<FetchResult<CategoriesResult>> {
+  const redisClient = context.client.redisTagClient;
   const apolloClient = context.client.apollo as ApolloClient<any>;
+
+  const cacheKey = 'API-C-ALL-CATEGORIES';
+  let cachedCategories = null;
+
+  if (redisClient && (cachedCategories = await redisClient.get(cacheKey))) {
+    return cachedCategories;
+  }
 
   const { getCategories } = context.extendQuery(
     customQuery, { getCategories: { query, variables: params } }
@@ -20,9 +29,18 @@ export default async function getCategories(
   const response = await apolloClient.query({
     query: gql`${getCategories.query}`,
     variables: getCategories.variables,
-    errorPolicy: 'all',
-    fetchPolicy: 'no-cache'
+    errorPolicy: 'all'
   });
+
+  delete response?.data?.cookie;
+  if (redisClient && response.data?.categories) {
+    redisClient.set(
+      cacheKey,
+      response,
+      [cacheKey],
+      { timeout: process.env.REDIS_TTL_CACHE_MAXIMUM ? randomIntegerBetween(Number(process.env.REDIS_TTL_CACHE_MINIMUM), Number(process.env.REDIS_TTL_CACHE_MAXIMUM)) : 86400 }
+    );
+  }
 
   return response;
 }
