5e7614217273f90f21df6ec58abbed2e76e82ab6
Leonardo Muniz <lmuniz@odoogap.com>
2023-07-15 15:36:59 -0300

    feat: finish all redis cart methods

--- packages/api-client/src/api/cartRedis/redisAddItemToCart.ts
+++ packages/api-client/src/api/cartRedis/redisAddItemToCart.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Context } from '@vue-storefront/core';
 
-export default async function redisAddItemToCart(context: Context, product: any): Promise<any> {
+export default async function redisAddItemToCart(context: Context, product: any, quantity = 1): Promise<any> {
   if (!context.req.session?.cart) {
     context.req.session. cart = {
       orderLines: []
@@ -15,5 +15,16 @@ export default async function redisAddItemToCart(context: Context, product: any)
   delete product.jsonLd;
   delete product.schemaAttributeValues;
 
-  context.req.session.cart.orderLines.push({ product });
+  const previousAddedItem = context.req.session.cart.orderLines?.find(item => item.product.id === product.id);
+  if (previousAddedItem) {
+    previousAddedItem.quantity += quantity;
+    context.req.session.cart.totalItemsInCart = context.req.session.cart.orderLines.length;
+    context.req.session.cart.totalItemsInCartWithQuantity = context.req.session.cart.orderLines.reduce((acc, item) => acc + item.quantity, 0);
+    return { data: context.req.session.cart };
+  }
+
+  context.req.session.cart.orderLines.push({ quantity: quantity, product, id: context.req.session.cart.orderLines.length + 1 });
+  context.req.session.cart.totalItemsInCart = context.req.session.cart.orderLines.length;
+
+  return { data: context.req.session.cart };
 }
