2c3e92e62ec183931452592a7dead6e2e8e342d8
inNetto <inetto.ferreira@gmail.com>
2025-04-15 14:48:53 -0300

    updating nuxt on doc

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ["sf-docs-base"],
+  compatibilityDate: "2025-04-15",
+  sitemap: {
+    enabled: false,
+  },
+  runtimeConfig: {
+    public: {
+      storefrontUi: true,
+      DOCS_EXAMPLES_VUE_PATH: process.env.NUXT_DOCS_EXAMPLES_VUE_PATH,
+      DOCS_EXAMPLES_REACT_PATH: process.env.NUXT_DOCS_EXAMPLES_REACT_PATH,
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://docs.alokai.com',
+      siteName: 'Odoo Alokai Integration Docs',
+      siteDescription: 'Fast, easy and powerful integration of Odoo with Alokai',
+    },
+  },
 });
