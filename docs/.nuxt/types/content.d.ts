96bd129520a8b31bc82457d41c351cad8970fb5e
Leonardo Muniz <lmuniz@odoogap.com>
2025-05-05 15:50:43 -0300

    chore: removing wrong pushed folder

--- docs/.nuxt/types/content.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '#content/server' {
-  const serverQueryContent: typeof import('/home/leonardo/Documents/PROJETOS/odoo/docs/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
-  const parseContent: typeof import('/home/leonardo/Documents/PROJETOS/odoo/docs/node_modules/@nuxt/content/dist/runtime/server').parseContent
-}
